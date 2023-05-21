40fd9959f66772157810fc248c688f3b2482aa8d
ChenHaoYan <chanhaoyan@126.com>
2023-05-21 16:04:55 +0800

    把版本号更改为自动获取

--- src/taskworker/__init__.py
+++ src/taskworker/__init__.py
@@ -1,8 +1,10 @@
+from importlib.metadata import version
+
 from . import cleanup as _cleanup
-from .defines import MAX_WORKER, Task, TypeWorker, Status, Error
+from .defines import MAX_WORKER, Error, Status, Task, TypeWorker
 from .setting import Setting, getGlobalSetting, setGlobalSetting
 from .taskWorker import TaskWorker
 from .util import Table, dict2list, list2dict, list2table
 from .workerPool import WorkerPoolThread
 
-__version__ = "0.0.1b"
+__version__ = version("TaskWorker")
